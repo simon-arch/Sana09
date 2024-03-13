dac480360b2602a24ea530a9872064b6e02e9929
Ващенко Артем Ігорович <ipz231_vai@student.ztu.edu.ua>
2024-03-13 22:02:50 +0200

    Implemented simple shop controller

---
+++ webapplication/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapplication.Models;
+
+namespace webapplication.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Shop()
+        {
+            return View();
+        }
+        public IActionResult Buy(int id)
+        {
+            Product product = new Product();
+            product.Id = id;
+            switch (id)
+            {
+                case 1: product.Name = "Apples"; break;
+                case 2: product.Name = "Bread"; break;
+                case 3: product.Name = "Milk"; break;
+                case 4: product.Name = "Eggs"; break;
+            }
+            product.Price = 50;
+            return View(product);
+        }
+    }
+}
